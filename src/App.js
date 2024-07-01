f3c12d86dd4a5cf65febaf12c4bd8b5e716d4d3a
Preetham Ananthkumar <preetham.mervin@gmail.com>
2024-07-01 11:42:30 +0100

    Refactored application for React and added initial Terminal layout

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Terminal from "./components/Terminal/Terminal";
+import "./App.css";
+
+const App = () => (
+  <div className="App">
+    <Terminal />
+  </div>
+);
+
+export default App;
